a5b17427f8d1b51d11158dc8d278dc2026814478
Bjwills7 <bjwills7@hotmail.com>
2024-05-29 14:53:22 -0500

    Add basic list traversal methods

--- linkedList.js
+++ linkedList.js
@@ -1,22 +1,22 @@
 const util = require("util");
 
 class LinkedList {
+  #size;
+
   constructor() {
     this.head = null;
+    this.#size = 0;
   }
 
   append(value) {
     if (this.head === null) {
       this.head = new Node(value);
     } else {
-      var nextNode = this.head;
+      var lastNode = this.tail;
 
-      while (nextNode.next !== null) {
-        nextNode = nextNode.next;
-      }
-
-      nextNode.next = new Node(value);
+      lastNode.next = new Node(value);
     }
+    this.#size++;
   }
 
   prepend(value) {
@@ -27,6 +27,71 @@ class LinkedList {
       node.next = this.head;
       this.head = node;
     }
+    this.#size++;
+  }
+
+  get size() {
+    return this.#size;
+  }
+
+  get getHead() {
+    return this.head;
+  }
+
+  get tail() {
+    var node = this.head;
+
+    while (node.next !== null) {
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  at(index) {
+    var node = this.head;
+
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  pop() {
+    var node = this.head;
+
+    while (node.next.next !== null) {
+      node = node.next;
+    }
+
+    node.next = null;
+  }
+
+  contains(value) {
+    var node = this.head;
+
+    while (node !== null) {
+      if (node.value === value) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
+  }
+
+  find(value) {
+    var node = this.head;
+    var index = 0;
+
+    while (node !== null) {
+      if (node.value === value) {
+        return index;
+      }
+      index++;
+      node = node.next;
+    }
+    return null;
   }
 }
 
@@ -44,3 +109,5 @@ list.append(3);
 list.append(4);
 
 console.log(util.inspect(list, { depth: null }));
+list.pop();
+console.log(list.find(7));
